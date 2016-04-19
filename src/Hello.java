44153793fc5f8b6bf7892b28f1e6dadebb2b5ef9
Ankur Puri <puri.ankur@yahoo.com>
2016-04-19 17:29:09 -0500

    Removed #ToDo

--- src/Hello.java
+++ src/Hello.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		//Added a comment
 		//System.out.println("Hello, World!");
